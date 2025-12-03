e47e69b0f8f32bda0064d401c67f33b591b61881
PowerSiderS.X DARK (KiLiDARK) <nikoladark87@gmail.com>
2025-12-03 21:35:43 +0100

    Update draw_util.h

--- cl_dll/include/draw_util.h
+++ cl_dll/include/draw_util.h
@@ -47,151 +47,166 @@ int Con_UtfProcessCharForce( int in );
 class DrawUtils
 {
 public:
-	static int DrawHudNumber(int x, int y, int iFlags, int iNumber,
-						 int r, int g, int b );
+        static int DrawHudNumber(int x, int y, int iFlags, int iNumber,
+                                                 int r, int g, int b );
 
-	static int DrawHudNumber2( int x, int y, bool DrawZero, int iDigits, int iNumber,
-						   int r, int g, int b);
+        static int DrawHudNumber2( int x, int y, bool DrawZero, int iDigits, int iNumber,
+                                                   int r, int g, int b);
 
-	static int DrawHudNumber2( int x, int y, int iNumber,
-						   int r, int g, int b);
+        static int DrawHudNumber2( int x, int y, int iNumber,
+                                                   int r, int g, int b);
 
-	static int DrawHudString(int x, int y, int iMaxX, const char *szString,
-						 int r, int g, int b, float scale = 0.0f, bool drawing = false );
+        static int DrawHudString(int x, int y, int iMaxX, const char *szString,
+                                                 int r, int g, int b, float scale = 0.0f, bool drawing = false );
 
-	static int DrawHudStringReverse( int xpos, int ypos, int iMinX, const char *szString,
-								 int r, int g, int b, float scale = 0.0f, bool drawing = false );
+        static int DrawHudStringReverse( int xpos, int ypos, int iMinX, const char *szString,
+                                                                 int r, int g, int b, float scale = 0.0f, bool drawing = false );
 
-	static inline int DrawHudNumberString( int xpos, int ypos, int iMinX, int iNumber,
-								int r, int g, int b, float scale = 0.0f )
-	{
-		char szString[16];
-		snprintf( szString, sizeof(szString), "%d", iNumber );
-		return DrawHudStringReverse( xpos, ypos, iMinX, szString, r, g, b, scale );
-	}
+        static inline int DrawHudNumberString( int xpos, int ypos, int iMinX, int iNumber,
+                                                                int r, int g, int b, float scale = 0.0f )
+        {
+                char szString[16];
+                snprintf( szString, sizeof(szString), "%d", iNumber );
+                return DrawHudStringReverse( xpos, ypos, iMinX, szString, r, g, b, scale );
+        }
 
-	static int HudStringLen( const char *szIt, float scale = 1 );
+        static int HudStringLen( const char *szIt, float scale = 1 );
 
-	// legacy shit came with Valve
-	static inline int GetNumWidth(int iNumber, int iFlags)
-	{
-		if ( iFlags & ( DHN_3DIGITS ) )
-			return 3;
+        // legacy shit came with Valve
+        static inline int GetNumWidth(int iNumber, int iFlags)
+        {
+                if ( iFlags & ( DHN_3DIGITS ) )
+                        return 3;
 
-		if ( iFlags & ( DHN_2DIGITS ) )
-			return 2;
+                if ( iFlags & ( DHN_2DIGITS ) )
+                        return 2;
 
-		if ( iNumber <= 0 )
-			return iFlags & DHN_DRAWZERO ? 1 : 0;
+                if ( iNumber <= 0 )
+                        return iFlags & DHN_DRAWZERO ? 1 : 0;
 
-		if ( iNumber < 10 )
-			return 1;
+                if ( iNumber < 10 )
+                        return 1;
 
-		if ( iNumber < 100 )
-			return 2;
+                if ( iNumber < 100 )
+                        return 2;
 
-		return 3;
-	}
+                return 3;
+        }
 
-	static inline int DrawConsoleString(int x, int y, const char *string)
-	{
-		if ( gHUD.hud_textmode->value )
-		{
-			int ret  = DrawHudString( x, y, 9999, (char *)string, color[0] * 255, color[1] * 255, color[2] * 255 );
-			color[0] = color[1] = color[2] = 1.0f;
-			return ret;
-		}
-		else
-			return gEngfuncs.pfnDrawConsoleString( x, y, (char *)string );
-	}
+        static inline int DrawConsoleString(int x, int y, const char *string)
+        {
+                if ( gHUD.hud_textmode->value )
+                {
+                        int ret  = DrawHudString( x, y, 9999, (char *)string, color[0] * 255, color[1] * 255, color[2] * 255 );
+                        color[0] = color[1] = color[2] = 1.0f;
+                        return ret;
+                }
+                else
+                        return gEngfuncs.pfnDrawConsoleString( x, y, (char *)string );
+        }
 
-	static inline void SetConsoleTextColor( float r, float g, float b )
-	{
-		if ( gHUD.hud_textmode->value )
-			color[0] = r, color[1] = g, color[2] = b;
-		else
-			gEngfuncs.pfnDrawSetTextColor( r, g, b );
-	}
+        static inline void SetConsoleTextColor( float r, float g, float b )
+        {
+                if ( gHUD.hud_textmode->value )
+                        color[0] = r, color[1] = g, color[2] = b;
+                else
+                        gEngfuncs.pfnDrawSetTextColor( r, g, b );
+        }
 
-	static inline void SetConsoleTextColor( unsigned char r, unsigned char g, unsigned char b )
-	{
-		if ( gHUD.hud_textmode->value )
-			color[0] = r / 255.0f, color[1] = g / 255.0f, color[2] = b / 255.0f;
-		else
-			gEngfuncs.pfnDrawSetTextColor( r / 255.0f, g / 255.0f, b / 255.0f );
-	}
+        static inline void SetConsoleTextColor( unsigned char r, unsigned char g, unsigned char b )
+        {
+                if ( gHUD.hud_textmode->value )
+                        color[0] = r / 255.0f, color[1] = g / 255.0f, color[2] = b / 255.0f;
+                else
+                        gEngfuncs.pfnDrawSetTextColor( r / 255.0f, g / 255.0f, b / 255.0f );
+        }
 
-	static inline int ConsoleStringLen(  const char *szIt )
-	{
-		if ( gHUD.hud_textmode->value )
-		{
-			return HudStringLen( (char *)szIt );
-		}
-		else
-		{
-			int _width;
-			int _height;
+        static inline int ConsoleStringLen(  const char *szIt )
+        {
+                if ( gHUD.hud_textmode->value )
+                {
+                        return HudStringLen( (char *)szIt );
+                }
+                else
+                {
+                        int _width;
+                        int _height;
 
-			gEngfuncs.pfnDrawConsoleStringLen( szIt, &_width, &_height );
-			return _width;
-		}
-	}
+                        gEngfuncs.pfnDrawConsoleStringLen( szIt, &_width, &_height );
+                        return _width;
+                }
+        }
 
-	static inline void ConsoleStringSize( const char *szIt, int *width, int *height )
-	{
-		if ( gHUD.hud_textmode->value )
-			*height = 13, *width = HudStringLen( (char *)szIt );
-		else
-			gEngfuncs.pfnDrawConsoleStringLen( szIt, width, height );
-	}
+        static inline void ConsoleStringSize( const char *szIt, int *width, int *height )
+        {
+                if ( gHUD.hud_textmode->value )
+                        *height = 13, *width = HudStringLen( (char *)szIt );
+                else
+                        gEngfuncs.pfnDrawConsoleStringLen( szIt, width, height );
+        }
 
-	static inline int TextMessageDrawChar( int x, int y, int number, int r, int g, int b, float scale = 0.0f )
-	{
-		int ret;
-		if( scale && g_iMobileAPIVersion )
-			ret = gMobileAPI.pfnDrawScaledCharacter( x, y, number, r, g, b, scale ) / gHUD.m_flScale;
-		else
-			ret = gEngfuncs.pfnDrawCharacter( x, y, number, r, g, b );
-		return ret;
-	}
+        static inline int TextMessageDrawChar( int x, int y, int number, int r, int g, int b, float scale = 0.0f )
+        {
+                int ret;
+                if( scale && g_iMobileAPIVersion )
+                        ret = gMobileAPI.pfnDrawScaledCharacter( x, y, number, r, g, b, scale ) / gHUD.m_flScale;
+                else
+                        ret = gEngfuncs.pfnDrawCharacter( x, y, number, r, g, b );
+                return ret;
+        }
 
-	static inline void UnpackRGB( int &r, int &g, int &b, const unsigned long ulRGB )
-	{
-		r = (ulRGB & 0xFF0000) >>16;
-		g = (ulRGB & 0xFF00) >> 8;
-		b = ulRGB & 0xFF;
-	}
+        static inline void UnpackRGB( int &r, int &g, int &b, const unsigned long ulRGB )
+        {
+                r = (ulRGB & 0xFF0000) >>16;
+                g = (ulRGB & 0xFF00) >> 8;
+                b = ulRGB & 0xFF;
+        }
 
-	static inline void ScaleColors( int &r, int &g, int &b, const int a )
-	{
-		r *= a / 255.0f;
-		g *= a / 255.0f;
-		b *= a / 255.0f;
-	}
+        static inline void ScaleColors( int &r, int &g, int &b, const int a )
+        {
+                r *= a / 255.0f;
+                g *= a / 255.0f;
+                b *= a / 255.0f;
+        }
 
-	static inline void DrawRectangle( int x, int y, int wide, int tall,
-						   int r = 0, int g = 0, int b = 0, int a = 153,
-						   bool drawStroke = true )
-	{
-		FillRGBABlend( x, y, wide, tall, r, g, b, a );
-		if ( drawStroke )
-		{
-			// TODO: remove this hardcoded hardcore
-			FillRGBA( x + 1,        y,            wide - 1, 1,        255, 140, 0, 255 );
-			FillRGBA( x,            y,            1,        tall - 1, 255, 140, 0, 255 );
-			FillRGBA( x + wide - 1, y + 1,        1,        tall - 1, 255, 140, 0, 255 );
-			FillRGBA( x,            y + tall - 1, wide - 1, 1,        255, 140, 0, 255 );
-		}
-	}
+        static inline void DrawRectangle( int x, int y, int wide, int tall,
+                                                   int r = 0, int g = 0, int b = 0, int a = 153,
+                                                   bool drawStroke = true )
+        {
+                FillRGBABlend( x, y, wide, tall, r, g, b, a );
+                if ( drawStroke )
+                {
+                        // TODO: remove this hardcoded hardcore
+                        FillRGBA( x + 1,        y,            wide - 1, 1,        255, 140, 0, 255 );
+                        FillRGBA( x,            y,            1,        tall - 1, 255, 140, 0, 255 );
+                        FillRGBA( x + wide - 1, y + 1,        1,        tall - 1, 255, 140, 0, 255 );
+                        FillRGBA( x,            y + tall - 1, wide - 1, 1,        255, 140, 0, 255 );
+                }
+        }
 
-	static void Draw2DQuad( float x1, float y1, float x2, float y2 );
-	static void DrawStretchPic( float x, float y, float w, float h,
-								float s1 = 0, float t1 = 0, float s2 = 1, float t2 = 1);
+        static inline void DrawRectangleExt( int x, int y, int wide, int tall,
+                                                      int r = 0, int g = 0, int b = 0, int a = 153,
+                                                      int stroke_r = 255, int stroke_g = 140, int stroke_b = 0,
+                                                      bool drawStroke = true, int cornerRadius = 2 )
+        {
+                FillRGBABlend( x, y, wide, tall, r, g, b, a );
+                if ( drawStroke )
+                {
+                        FillRGBA( x + cornerRadius, y,                    wide - (cornerRadius * 2), 1,        stroke_r, stroke_g, stroke_b, 255 );
+                        FillRGBA( x,                y + cornerRadius,     1,        tall - (cornerRadius * 2), stroke_r, stroke_g, stroke_b, 255 );
+                        FillRGBA( x + wide - 1,     y + cornerRadius,     1,        tall - (cornerRadius * 2), stroke_r, stroke_g, stroke_b, 255 );
+                        FillRGBA( x + cornerRadius, y + tall - 1,         wide - (cornerRadius * 2), 1,        stroke_r, stroke_g, stroke_b, 255 );
+                }
+        }
+
+        static void Draw2DQuad( float x1, float y1, float x2, float y2 );
+        static void DrawStretchPic( float x, float y, float w, float h,
+                                                                float s1 = 0, float t1 = 0, float s2 = 1, float t2 = 1);
 
 
 private:
-	// console string color
-	static float color[3];
+        // console string color
+        static float color[3];
 };
 
 #endif // DRAW_UTIL_H
